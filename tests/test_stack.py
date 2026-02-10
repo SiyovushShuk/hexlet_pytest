202043899edfa64d7de1808f3eaf46a43ee95e4a
Seva <shukurovsiyovush1@gmail.com>
2026-02-10 16:15:46 +0500

    Add stack test

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -41,6 +41,7 @@ def test_emptiness():
 
 def test_pop_with_empty_stack():
     stack = []
-
+    # проверить что вызывается конкретное исключение можно с помощью конструкции with pytest.raises()
+    # если внутри блока вызовется исключение, то тест будет пройден
     with pytest.raises(IndexError):
         stack.pop()
