5e9e62d8db1d417d51c4e1e2ad192c91a26e3720
Seva <shukurovsiyovush1@gmail.com>
2026-02-11 09:52:54 +0500

    Add pytest-cov

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,5 @@
 from hexlet_pytest.example import reverse
+import pytest
 
 
 def test_reverse():
@@ -7,3 +8,12 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse("") == ""
+
+@pytest.fixture
+def arr():
+    return [1, 2, 3, 4]
+
+
+def test_example(arr):
+    assert [1, 2, 3, 4] == arr
+    print('Hello')
